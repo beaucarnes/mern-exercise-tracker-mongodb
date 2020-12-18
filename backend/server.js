fff84e866d1173fc39101698041d61c75b432411
RobyCigar <69680330+RobyCigar@users.noreply.github.com>
2020-12-18 11:18:48 +0700

    Adding { useUnifiedTopology:true }
    when i read along your tutorial i found an error, and then solved with this answer 
    https://stackoverflow.com/questions/57895175/server-discovery-and-monitoring-engine-is-deprecated 
    Please also update your tutorial on medium, so the other reader don't confuse with this error
    thank you :smiling_face_with_three_hearts:

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
