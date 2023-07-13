41efaca75a5127cc20961e057143d9fa39189076
prakharraj1302 <63489114+prakharraj1302@users.noreply.github.com>
2023-07-13 16:48:56 +0530

    Update edit-exercise.component.js

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -23,7 +23,7 @@ export default class EditExercise extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/exercises/'+this.props.match.params.id)
+    axios.get('https://mern-backend-1-render.onrender.com/exercises/'+this.props.match.params.id)
       .then(response => {
         this.setState({
           username: response.data.username,
@@ -36,7 +36,7 @@ export default class EditExercise extends Component {
         console.log(error);
       })
 
-    axios.get('http://localhost:5000/users/')
+    axios.get('https://mern-backend-1-render.onrender.com/users/')
       .then(response => {
         if (response.data.length > 0) {
           this.setState({
@@ -86,7 +86,7 @@ export default class EditExercise extends Component {
 
     console.log(exercise);
 
-    axios.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
+    axios.post('https://mern-backend-1-render.onrender.com/exercises/update/' + this.props.match.params.id, exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
